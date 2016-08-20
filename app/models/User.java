41c352aedea81506ac65391bd08a1cac39f4a083
Live session user <sandeep.k@betsol.com>
2016-08-20 10:02:36 +0000

    successfully programmed model

---
+++ app/models/User.java
@@ -0,0 +1,45 @@
+package models;
+
+import com.avaje.ebean.Model;
+
+import javax.jws.soap.SOAPBinding;
+import javax.persistence.*;
+import javax.validation.constraints.AssertFalse;
+import java.util.List;
+import java.util.Set;
+
+/**
+ * Created by lubuntu on 8/20/16.
+ */
+@Entity
+public class User extends Model {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    public Long id;
+    public String email;
+    public String password;
+
+    @OneToMany(mappedBy ="sender" )
+    public List<ConnectionRequest> connectionRequestSent;
+
+    @OneToMany(mappedBy = "receiver")
+    public List<ConnectionRequest> connectionRequestReceived;
+
+    @OneToOne
+    public Profile profile;
+
+    @ManyToMany
+    @JoinTable(name = "user_connections",
+    joinColumns= {
+            @JoinColumn(name = "user_id")
+                    },
+            inverseJoinColumns = {
+            @JoinColumn(name = "connection_id")
+                        }
+            )
+
+    public Set<User> connections;
+
+
+
+}
